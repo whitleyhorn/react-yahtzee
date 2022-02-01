b780dce82c6c2973040781ae30fd38b5a0c8dbe4
Whitley Horn <whitleyblaine@protonmail.com>
2022-01-31 20:41:31 -0500

    use font awesome icons

--- src/Die.js
+++ src/Die.js
@@ -7,20 +7,23 @@ class Die extends Component {
       this.handleClick = this.handleClick.bind(this);
   }
 
+  valToWord(){
+      let wordMap = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six"};
+      return wordMap[this.props.val];
+  }
+
   handleClick(){
       this.props.handleClick(this.props.idx);
   }
 
   render() {
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
+      <i
+        className={`fa-solid fa-dice-${this.valToWord()}`}
+        style={{ fontSize: '48px', color: this.props.locked ? '#a8a8a8' : 'black' }}
         onClick={this.handleClick}
         disabled={this.props.rollsLeft <= 0}
-      >
-        {this.props.val}
-      </button>
+      />
     );
   }
 }
